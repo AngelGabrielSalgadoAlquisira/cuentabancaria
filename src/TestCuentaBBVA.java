5a91aee529a93969b450063592545ef08ea8205a
Angel Gabriel <alquisira2004@gmail.com>
2023-03-09 17:47:49 -0600

    Se crea la clase cuenta

---
+++ src/TestCuentaBBVA.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class TestCuentaBBVA {
+    public static void main(String[] args) throws IOException{
+
+        CuentaBBVA Account = new CuentaBBVA("angel",8989898,890);
+
+        Account.Menu();
+
+
+
+    }
+
+
+}
+
